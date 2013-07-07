50a3a2971348c0039f3b42bb0093e5fb6d0ef457
jose delgado <jose.a.delgado.lopez@gmail.com>
2013-07-07 18:46:39 +0000

    Upadate 3 web.js

--- web.js
+++ web.js
@@ -1,6 +1,7 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
+var fs= require('fs');
 var buffer = new Buffer(fs.readFileSync("index.html"), "utf-8");
 
 app.get('/', function(request, response) {
