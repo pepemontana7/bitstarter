ec8f8e470a10344332394ca75bf04ee97dafcabb
jose delgado <jose.a.delgado.lopez@gmail.com>
2013-07-07 18:41:59 +0000

    Upadated web.js

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
+var buffer = new Buffer(fs.readFileSync("index.html"), "utf-8");
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(buffer.toString('utf-8'));
+    
 });
 
 var port = process.env.PORT || 5000;
